e5e51aba3ff513b3252894c81e33cdcc0d2ad830
jonnys1516 <39161533+jonnys1516@users.noreply.github.com>
2018-05-24 12:01:59 -0300

    enviando arquivos

---
+++ src/formularios/principal.java
@@ -0,0 +1,12 @@
+package formularios;
+
+import formularios.FormularioPrincipal;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		//Instanciar
+		FormularioPrincipal f = new FormularioPrincipal();
+	}
+}
